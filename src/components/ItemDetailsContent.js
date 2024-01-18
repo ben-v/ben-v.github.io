4633ed831da5fe5d3ad2ee9afdd7e4807879c967
Ben <8211835+ben-v@users.noreply.github.com>
2024-01-18 13:17:30 -0700

    Remove materialize packages for now.

--- src/components/ItemDetailsContent.js
+++ src/components/ItemDetailsContent.js
@@ -1,6 +1,6 @@
-import M from "materialize-css";
+// import M from "materialize-css";
 import { memo, useEffect, useRef } from "react";
-import { Carousel } from "react-materialize";
+// import { Carousel } from "react-materialize";
 
 // export const LocalVideo = ({ playing }) => {
 //   const vidRef = useRef(null);
@@ -29,42 +29,43 @@ const ProjectSlider = () => {
   }, []);
 
   return (
-    <Carousel
-      options={{
-        fullWidth: true,
-        indicators: true,
-      }}
-      className="carousel carousel-slider"
-    >
-      <a className="carousel-item" href="#one!">
-        <img
-          className="responsive-img"
-          src="images/projects/project-2.jpg"
-          alt="project"
-        />
-      </a>
-      <a className="carousel-item" href="#two!">
-        <img
-          className="responsive-img"
-          src="images/projects/project-1.jpg"
-          alt="project"
-        />
-      </a>
-      <a className="carousel-item" href="#three!">
-        <img
-          className="responsive-img"
-          src="images/projects/project-3.jpg"
-          alt="project"
-        />
-      </a>
-      <a className="carousel-item" href="#four!">
-        <img
-          className="responsive-img"
-          src="images/projects/project-4.jpg"
-          alt="project"
-        />
-      </a>
-    </Carousel>
+    <div></div>
+    // <Carousel
+    //   options={{
+    //     fullWidth: true,
+    //     indicators: true,
+    //   }}
+    //   className="carousel carousel-slider"
+    // >
+    //   <a className="carousel-item" href="#one!">
+    //     <img
+    //       className="responsive-img"
+    //       src="images/projects/project-2.jpg"
+    //       alt="project"
+    //     />
+    //   </a>
+    //   <a className="carousel-item" href="#two!">
+    //     <img
+    //       className="responsive-img"
+    //       src="images/projects/project-1.jpg"
+    //       alt="project"
+    //     />
+    //   </a>
+    //   <a className="carousel-item" href="#three!">
+    //     <img
+    //       className="responsive-img"
+    //       src="images/projects/project-3.jpg"
+    //       alt="project"
+    //     />
+    //   </a>
+    //   <a className="carousel-item" href="#four!">
+    //     <img
+    //       className="responsive-img"
+    //       src="images/projects/project-4.jpg"
+    //       alt="project"
+    //     />
+    //   </a>
+    // </Carousel>
   );
 };
 export default memo(ProjectSlider);
